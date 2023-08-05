d59388e74490a50277066e702c4ba7a1bcc5276b
Roman80-IT <127478871+Roman80-IT@users.noreply.github.com>
2023-08-05 15:41:11 +0000

    Deploying to gh-pages from @ Roman80-IT/goit-js-hw-09@57dcab00db0713f689a6c91539985287b130b280 🚀

---
+++ 01-color-switcher.bc730577.js
@@ -0,0 +1,3 @@
+//! ------------------------------------------------------------------------------------------
+const t=document.querySelector("button[data-start]");document.querySelector("button[data-stop]");t.addEventListener("click",onStartBtnClick);
+//# sourceMappingURL=01-color-switcher.bc730577.js.map
