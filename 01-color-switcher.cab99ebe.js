e5d7c924c6ad41b452bfe250ccc02b79248b7a14
Roman80-IT <127478871+Roman80-IT@users.noreply.github.com>
2023-08-05 22:33:59 +0000

    Deploying to gh-pages from @ Roman80-IT/goit-js-hw-09@53d10a3a6a22469d0034657246cdef06f578b2ef 🚀

--- 01-color-switcher.cab99ebe.js
+++ 01-color-switcher.cab99ebe.js
@@ -1,5 +1,5 @@
 !function(){
-//! ------------------------------------------------------------------------------------------
+//! ---------------------------   ВАРІАНТ 1   -------------------------------
 //! ---------------------------   ВАРІАНТ 2   -------------------------------
 var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,a=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(a)}));var a=null}();
-//# sourceMappingURL=01-color-switcher.55a13f13.js.map
+//# sourceMappingURL=01-color-switcher.cab99ebe.js.map
