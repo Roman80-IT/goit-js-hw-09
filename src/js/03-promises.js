b18c3356e77134139f34b95d4e36176db3f72861
Roman <jrj80mail@gmail.com>
2023-08-07 15:53:29 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -90,6 +90,75 @@
 //! ------------------------------   ВАРІАНТ 3   ----------------------------------
 //* Замість інпут, тут кнопка дізейбл:
 
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// const $form = document.querySelector('.form');
+// const $formInputs = {
+//   delay: document.querySelector('input[name="delay"]'),
+//   step: document.querySelector('input[name="step"]'),
+//   amount: document.querySelector('input[name="amount"]'),
+// };
+// const $startBtn = document.querySelector('button[type="submit"]');
+// const $resetBtn = document.getElementById('resetBtn');
+// let isPromiseRunning = false;
+
+// //* Деактивуємо кнопку "Start" при завантаженні сторінки
+// $startBtn.disabled = true;
+
+// //* Обробник події для подання форми:
+// $form.addEventListener('submit', event => {
+//   event.preventDefault();
+
+//   const delay = Number($formInputs.delay.value);
+//   const step = Number($formInputs.step.value);
+//   const amount = Number($formInputs.amount.value);
+
+//   if (step < 0 || delay < 0 || amount <= 0) {
+//     Notify.failure(
+//       'Incorrect values ​​of entered parameters! Некоректні значення введених параметрів!'
+//     );
+//     return;
+//   }
+
+//   //* Запобігаємо старту нових промісів, якщо генератор уже працює
+//   if (isPromiseRunning) {
+//     Notify.failure('Генератор вже запущений!');
+//     return;
+//   }
+
+//   isPromiseRunning = true;
+//   $startBtn.disabled = true; //* Деактивуємо кнопку "Start" під час роботи генератора
+
+//   for (let i = 0; i < amount; i += 1) {
+//     const promiseDelay = delay + i * step;
+
+//     createPromise(i + 1, promiseDelay)
+//       .then(value => {
+//         Notify.success(value);
+//       })
+//       .catch(err => {
+//         Notify.failure(err);
+//       });
+//   }
+
+//   //* Після завершення промісів, активуємо кнопку "Start" та встановлюємо змінну isPromiseRunning в false
+//   setTimeout(() => {
+//     isPromiseRunning = false;
+//     $startBtn.disabled = false;
+//   }, delay + (amount - 1) * step + 100); //* Запасний час на виконання останнього проміса
+// });
+
+// $resetBtn.addEventListener('click', event => {
+//   event.preventDefault();
+//   //* Скидаємо значення полів вводу на стандартні
+//   $formInputs.delay.disabled = false;
+//   $formInputs.delay.value = 1500;
+//   $formInputs.step.value = 1000;
+//   $formInputs.amount.value = 5;
+// });
+
+//! ------------------------------   ВАРІАНТ 4   ----------------------------------
+
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const $form = document.querySelector('.form');
@@ -98,14 +167,10 @@ const $formInputs = {
   step: document.querySelector('input[name="step"]'),
   amount: document.querySelector('input[name="amount"]'),
 };
-const $startBtn = document.querySelector('button[type="submit"]');
-const $resetBtn = document.getElementById('resetBtn');
-let isPromiseRunning = false;
 
-//* Деактивуємо кнопку "Start" при завантаженні сторінки
-$startBtn.disabled = true;
+const $submitButton = document.querySelector('button[type="submit"]');
+const $resetButton = document.querySelector('button[type="reset"]');
 
-//* Обробник події для подання форми:
 $form.addEventListener('submit', event => {
   event.preventDefault();
 
@@ -114,20 +179,12 @@ $form.addEventListener('submit', event => {
   const amount = Number($formInputs.amount.value);
 
   if (step < 0 || delay < 0 || amount <= 0) {
-    Notify.failure(
-      'Incorrect values ​​of entered parameters! Некоректні значення введених параметрів!'
-    );
+    Notify.failure('Некоректні значення введених параметрів.');
     return;
   }
 
-  //* Запобігаємо старту нових промісів, якщо генератор уже працює
-  if (isPromiseRunning) {
-    Notify.failure('Генератор вже запущений!');
-    return;
-  }
-
-  isPromiseRunning = true;
-  $startBtn.disabled = true; //* Деактивуємо кнопку "Start" під час роботи генератора
+  // Деактивуємо кнопку "Submit" під час генерації промісів
+  $submitButton.disabled = true;
 
   for (let i = 0; i < amount; i += 1) {
     const promiseDelay = delay + i * step;
@@ -138,21 +195,18 @@ $form.addEventListener('submit', event => {
       })
       .catch(err => {
         Notify.failure(err);
+      })
+      .finally(() => {
+        // Після завершення генерації промісів активуємо кнопку "Submit" знову
+        if (i === amount - 1) {
+          $submitButton.disabled = false;
+        }
       });
   }
-
-  //* Після завершення промісів, активуємо кнопку "Start" та встановлюємо змінну isPromiseRunning в false
-  setTimeout(() => {
-    isPromiseRunning = false;
-    $startBtn.disabled = false;
-  }, delay + (amount - 1) * step + 100); //* Запасний час на виконання останнього проміса
 });
 
-$resetBtn.addEventListener('click', event => {
-  event.preventDefault();
-  //* Скидаємо значення полів вводу на стандартні
-  $formInputs.delay.disabled = false;
-  $formInputs.delay.value = 1500;
-  $formInputs.step.value = 1000;
-  $formInputs.amount.value = 5;
+$resetButton.addEventListener('click', event => {
+  // Скидуємо значення і робимо інпути доступними для обрання промісів
+  $form.reset();
+  $submitButton.disabled = false;
 });
