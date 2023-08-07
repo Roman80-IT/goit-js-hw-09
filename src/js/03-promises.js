fa2e96c590036e45cbd191a87928b04a8a27e882
Roman <jrj80mail@gmail.com>
2023-08-07 19:41:07 +0300

    2

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -159,66 +159,6 @@
 
 //! ------------------------------   ВАРІАНТ 4   ----------------------------------
 
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
-// const $form = document.querySelector('.form');
-// const $formInputs = {
-//   delay: document.querySelector('input[name="delay"]'),
-//   step: document.querySelector('input[name="step"]'),
-//   amount: document.querySelector('input[name="amount"]'),
-// };
-
-// const $submitButton = document.querySelector('button[type="submit"]');
-// const $resetButton = document.querySelector('button[type="reset"]');
-
-// $form.addEventListener('submit', event => {
-//   event.preventDefault();
-
-//   const delay = Number($formInputs.delay.value);
-//   const step = Number($formInputs.step.value);
-//   const amount = Number($formInputs.amount.value);
-
-//   if (step < 0 || delay < 0 || amount <= 0) {
-//     Notify.failure('Некоректні значення введених параметрів.');
-//     return;
-//   }
-
-//   $submitButton.disabled = true; // Деактивація кнопки "Submit"
-//   $resetButton.disabled = true; // Деактивація кнопки "Reset"
-
-//   let promisesToExecute = []; // Зберігаємо проміси для майбутнього виконання
-
-//   for (let i = 0; i < amount; i += 1) {
-//     const promiseDelay = delay + i * step;
-
-//     const promise = createPromise(i + 1, promiseDelay)
-//       .then(value => {
-//         Notify.success(value);
-//       })
-//       .catch(err => {
-//         Notify.failure(err);
-//       })
-//       .finally(() => {
-//         // Після завершення промісу видаляємо його з масиву промісів
-//         promisesToExecute = promisesToExecute.filter(p => p !== promise);
-
-//         // Після виконання всіх промісів активуємо кнопки "Submit" і "Reset" знову
-//         if (promisesToExecute.length === 0) {
-//           $submitButton.disabled = false;
-//           $resetButton.disabled = false;
-//         }
-//       });
-
-//     promisesToExecute.push(promise);
-//   }
-// });
-
-// $resetButton.addEventListener('click', event => {
-//   $form.reset(); // Скидуємо значення форми
-// });
-
-//! ------------------------------   ВАРІАНТ 5   ----------------------------------
-
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const $form = document.querySelector('.form');
@@ -227,8 +167,13 @@ const $formInputs = {
   step: document.querySelector('input[name="step"]'),
   amount: document.querySelector('input[name="amount"]'),
 };
-const $submitBtn = document.getElementById('submitBtn');
+
 const $resetBtn = document.getElementById('resetBtn');
+$resetBtn.addEventListener('click', () => {
+  $formInputs.delay.value = '1000'; //* Значення по замовчуванню
+  $formInputs.step.value = '500';
+  $formInputs.amount.value = '5';
+});
 
 $form.addEventListener('submit', event => {
   event.preventDefault();
@@ -242,38 +187,17 @@ $form.addEventListener('submit', event => {
     return;
   }
 
-  // Disable the Submit button while promises are being generated
-  $submitBtn.disabled = true;
-  $resetBtn.disabled = true;
-
-  const promises = [];
-
   for (let i = 0; i < amount; i += 1) {
     const promiseDelay = delay + i * step;
-    promises.push(createPromise(i + 1, promiseDelay));
-  }
 
-  Promise.all(promises)
-    .then(values => {
-      for (const value of values) {
+    createPromise(i + 1, promiseDelay)
+      .then(value => {
         Notify.success(value);
-      }
-    })
-    .catch(err => {
-      Notify.failure(err);
-    })
-    .finally(() => {
-      // Enable the Submit button after all promises are resolved
-      $submitBtn.disabled = false;
-      $resetBtn.disabled = false;
-    });
-});
-
-// Reset button functionality
-$resetBtn.addEventListener('click', () => {
-  $formInputs.delay.value = '1000';
-  $formInputs.step.value = '500';
-  $formInputs.amount.value = '5';
+      })
+      .catch(err => {
+        Notify.failure(err);
+      });
+  }
 });
 
 function createPromise(position, delay) {
